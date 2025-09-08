af115778eaef4f2b0ac9509b957ed3d9cdbede17
saisrikarmeda <sai.srikar.meda@elementalconcept.com>
2025-09-08 08:33:22 +0100

    Adding got sample music (working)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookListComponent } from './features/book-list-component/book-list.component';
 import { BookDetailComponent } from './features/book-detail-component/book-detail.component';
@@ -16,6 +16,42 @@ import { CharacterModalComponent } from './features/character-modal-component/ch
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'A Song of Ice and Fire Books';
+  private audio = new Audio();
+  isPlaying = false;
+
+  ngOnInit() {
+    this.audio.src = 'got-theme.mp3';
+    this.audio.loop = true;
+    this.audio.volume = 0.3;
+
+    this.audio.addEventListener('loadstart', () => console.log('Audio loading started'));
+    this.audio.addEventListener('canplay', () => console.log('Audio can play'));
+    this.audio.addEventListener('error', (e) => console.error('Audio error:', e));
+  }
+
+  async playBackgroundMusic() {
+    try {
+      if (this.isPlaying) {
+        this.pauseMusic();
+      } else {
+        console.log('Attempting to play audio...');
+        console.log('Audio src:', this.audio.src);
+
+        await this.audio.play();
+        this.isPlaying = true;
+        console.log('Audio playing successfully');
+      }
+    } catch (error) {
+      console.error('Failed to play audio:', error);
+      alert('Could not play audio. Check console for details.');
+    }
+  }
+
+  pauseMusic() {
+    this.audio.pause();
+    this.isPlaying = false;
+    console.log('Audio paused');
+  }
 }
